67490ffdcb65ef836f732f46e31540eed92bbf67
houyanliang888 <1691008494@qq.com>
2026-08-26 10:08:14 +0800

    升级sw缓存版本v2，强制PWA更新

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='zhuangji-zhangben-v1';
+const CACHE='zhuangji-zhangben-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-512.png','./icon-192.png','./icon-180.png','./icon.svg'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
